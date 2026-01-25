f29fa34a8472571bf3cc893218ec741e7795c7f6
Shreyanshjain1008 <jainshreyansh1411@gmail.com>
2026-01-25 12:22:29 +0530

    updated

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -10,21 +10,42 @@ from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
+# @router.post("/register")
+# def register(data: RegisterRequest, db: Session = Depends(get_db)):
+#     if db.query(User).filter(User.email == data.email).first():
+#         raise HTTPException(status_code=400, detail="Email already exists")
+
+#     user = User(
+#         name=data.name,
+#         email=data.email,
+#         password_hash=hash_password(data.password)
+#     )
+#     db.add(user)
+#     db.commit()
+#     db.refresh(user)
+
+#     return {"message": "User registered successfully"}
 @router.post("/register")
 def register(data: RegisterRequest, db: Session = Depends(get_db)):
-    if db.query(User).filter(User.email == data.email).first():
-        raise HTTPException(status_code=400, detail="Email already exists")
+    try:
+        if db.query(User).filter(User.email == data.email).first():
+            raise HTTPException(status_code=400, detail="Email already exists")
 
-    user = User(
-        name=data.name,
-        email=data.email,
-        password_hash=hash_password(data.password)
-    )
-    db.add(user)
-    db.commit()
-    db.refresh(user)
+        user = User(
+            name=data.name,
+            email=data.email,
+            password_hash=hash_password(data.password)
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
 
-    return {"message": "User registered successfully"}
+        return {"message": "User registered successfully"}
+
+    except Exception as e:
+        print("REGISTER ERROR >>>", repr(e))
+        raise HTTPException(status_code=500, detail="Register failed")
+
 
 @router.post("/login")
 def login(data: LoginRequest, db: Session = Depends(get_db)):
