3a48d451ac4af9128224ded2c6a60d6e11af4fac
Shreyanshjain1008 <jainshreyansh1411@gmail.com>
2026-01-27 23:26:18 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="CloudVault API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cloud-vault-psi.vercel.app/"],
+    allow_origins=["https://cloud-vault-psi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
